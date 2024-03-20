c4ffbdc1fbca59e0ab4e6bf76ce2f95f721df2c0
Samira Gibson <saga44studio@gmail.com>
2024-03-20 10:49:45 -0400

    Initial commit to ardd repo

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import SpaceTime from './components/SpaceTime.vue';
+//import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  
+  <div id="slot1">
+    <div id="titles">
+    <h3>Quantum Visual Experiments</h3>
+    </div>
+    <div id="info">
+      <h3>Space  Time</h3>
+    <p>The fundamental concepts of classical physics, space, time, mass, and derived concepts.</p>
+    </div>
+  <div id="spaceContainer">
+    <SpaceTime />
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+#slot1 {
+ display: flex 2fr 2fr 2fr;
+ overflow-x: hidden;
+}
+#titles {
+  position: absolute;
+	top: 100px;
+  left: 100px;
+	text-align: center;
+	z-index: 100;
+	display:block;
+  color: #242526;
+  background-color: rgba(255, 255, 255, 0.60);
+  border-radius: 25px;
+  padding: 0 30px;
+}
+#info {
+  position: absolute;
+	top: 50;
+  bottom: 45%;
+  left: 50;
+  right: 10%;
+	text-align: center;
+	z-index: 100;
+	display:flex;
+  color: #242526;
+  background-color: rgba(255, 255, 255, 0.60);
+  border-radius: 25px;
+  padding: 0 30px;
+  max-width: 270px;
+}
+#info h3{
+  align-self: center;
+  padding-right: 5%;
+}
+
+#info p {
+  font-size: small;
+  align-self: center;
+}
+</style>
---
+++ src/components/SpaceTime.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { TresCanvas, useRenderLoop } from '@tresjs/core';
+import { shallowRef, Suspense } from 'vue';
+import { MapControls, Stars, Line2, Smoke } from '@tresjs/cientos';
+import { DoubleSide } from 'three';
+
+const yRotate = shallowRef(0);
+const obRotate = shallowRef(1);
+const camPos = shallowRef(2);
+//const obPos = shallowRef(3);
+useRenderLoop().onLoop(({delta}) => {
+    yRotate.value += 0.02 * delta;
+    obRotate.value += -0.5 * delta;
+    camPos.value -= 0.5 * delta;
+    //obPos.value += -0.55 * delta;
+});
+
+
+
+</script>
+
+<template>
+    <TresCanvas id="canvas" window-size alpha shadows clear-color="#242526" >
+        <TresAmbientLight 
+            point
+        />
+        <TresPerspectiveCamera visible :position="[3, 7.5, 12]" />
+        <Line2
+        :points="[[0, 0, 0], [3, 1.58, 0], [5, 0.8660, 0]]"
+        :line-width="10"
+        color="#82dbc5"
+        />
+        <Stars
+            :count="20000"
+            :rotation="[0, yRotate, 0]"
+         />
+         <Suspense>
+            <Smoke 
+            :speed="0.3"
+            :segments="20"
+            color="#ffffff"
+            :depth="10"
+            :width="50"
+            :depth-test="true"
+            />
+        </Suspense>
+        <MapControls />
+        <TresMesh :position="[3,3,0]">
+            <TresSphereGeometry :scale="[0.5, 0.5, 0.5]"/>
+            <TresMeshPhysicalMaterial color="red" />
+            <DoubleSide />
+        </TresMesh>
+        <TresGridHelper>
+            <TresMeshBasicMaterial color="white" />
+        </TresGridHelper>
+
+    </TresCanvas>
+</template>
+
+<style scoped>
+</style>
